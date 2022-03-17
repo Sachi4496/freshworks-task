f700b7697dedc28b0ac53d4f6042197c8f129053
Sachidanand Pradhan <53556482+Sachi4496@users.noreply.github.com>
2022-03-17 14:35:18 +0530

    commit

---
+++ components/nav.js
@@ -0,0 +1,15 @@
+function nav() {
+    return `<a href="index.html"><img
+    src="https://confluence.freshworks.com/download/attachments/32772/atl.site.logo?version=2&modificationDate=1557916724023&api=v2"
+    alt="freshworks"></a>
+    <div id="navBar">
+    <h3><a href="index.html">Home</a></h3>
+    <h3><a href="about.html">About</a></h3>
+    <h3><a href="service.html">Service</a></h3>
+    <h3><a href="contact.html">Contact</a></h3>
+    <h3><a href="login.html">Login</a></h3>
+    </div>`
+
+}
+
+export default nav
