7461b458e2f4b3c0c271c43779fc642fb394e42c
Sachidanand Pradhan <53556482+Sachi4496@users.noreply.github.com>
2022-03-17 17:38:23 +0530

    commit

--- components/nav.js
+++ components/nav.js
@@ -3,7 +3,7 @@ function nav() {
 </a>
 <div id="navBar">
     <h3><a href="receipe.html">Get receipe</a></h3>
-    <h3><a href="latestReceipe.html">show latest receipe</a></h3>
+    <h3><a href="latestRecepie.html">show latest receipe</a></h3>
 </div>`
 
 }
